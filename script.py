2e7dc89a109a325b316c2e36e5a2858627269fa1
setronn <setron34@gmail.com>
2022-12-18 10:26:52 -0500

    add async requests

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 import subprocess
 import re
-import requests
+import asyncio
+import aiohttp
 import html
-import sys
+import time
 
 flag_find_mail_addresses = False
 
@@ -18,7 +19,7 @@ class Bash_features:
 
     def execute_bash(command):
         try:
-            return subprocess.check_output(command.split(" ")).decode("utf-8").split('\n')
+            return subprocess.check_output(command.split(" "), stderr=subprocess.DEVNULL).decode("utf-8").split('\n')
         except subprocess.CalledProcessError as e:
             print(f"Bash command error ({command}): {e.output}")
             return None
@@ -40,28 +41,27 @@ class Bash_features:
         ip_addresses = [ip for ip in ip_addresses if ip]
         return ip_addresses
 
-    def parse_title_by_url(url):
-        html_encoded_response = requests.get(url, verify=False).text
-        response = html.unescape(html_encoded_response)
+    async def parse_title_by_url(url):
         try:
-            return re.search(r"<title>(.*)<\/title>", response, re.DOTALL).group(1)
-        except AttributeError as e:
-            print (f"Cannot find <title> in: {url}.")
-            return None
+            timeout = aiohttp.ClientTimeout(total=3)
+            async with aiohttp.ClientSession(timeout=timeout) as session:
+                async with session.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"}, ssl=False) as response:
+                    response_text = await response.text()
+                    response_text = html.unescape(response_text)
+                    return re.search(r"<title>(.*)<\/title>", response_text, re.DOTALL).group(1).replace('\n', ' ')
+        except Exception as e:
+            print(e)
+            return ""
 
 
 class Domain_dict:
 
-    __domain_dict = {}
-    requests.packages.urllib3.disable_warnings()
-
-    # There is should be a valid domain
     def __init__(self, domain_name):
+        self.__domain_dict = {}
         self.__domain_dict.update({"domain_name": Bash_features.parse_domain_name(domain_name)})
-        self.find_mail_subdomains()
-        self.find_open_web_servers()
+        self.__find_mail_subdomains()
         if flag_find_mail_addresses:
-            self.find_mail_addresses()
+            self.__find_mail_addresses()
     
     def __parse_mx_records(self, line):
         if line == '':
@@ -72,7 +72,7 @@ class Domain_dict:
             print (f"Cannot parse mail domain in a string: {line}")
             return None
 
-    def find_second_level_domain(self):
+    def __find_second_level_domain(self):
         domain_name = self.__domain_dict["domain_name"]
         try:
             second_level_domain = re.search(r"([\w\-]*\.[\w\-]*)$", domain_name).group(1)
@@ -81,9 +81,9 @@ class Domain_dict:
             print (f"Something weird has happened. Cannot parse 2 level domain from a domain: {domain_name}")
         
     # Find mail subdomains through digging MX records
-    def find_mail_subdomains(self):
+    def __find_mail_subdomains(self):
         if not "second_level_domain" in self.__domain_dict:
-            self.find_second_level_domain()
+            self.__find_second_level_domain()
         second_level_domain = self.__domain_dict["second_level_domain"]
         dig_short_mx_answer = Bash_features.execute_bash(f"dig +short mx {second_level_domain}")
         mail_subdomains = []
@@ -96,9 +96,9 @@ class Domain_dict:
         mail_subdomains = [*set(mail_subdomains)]
         self.__domain_dict.update({"mail_subdomains": mail_subdomains})
 
-    def find_mail_addresses(self):
+    def __find_mail_addresses(self):
         if not "second_level_domain" in self.__domain_dict:
-            self.find_second_level_domain()
+            self.__find_second_level_domain()
         second_level_domain = self.__domain_dict["second_level_domain"]
         emailharvester_answer = Bash_features.execute_bash(f"emailharvester -d {second_level_domain}")
         # Locate string "[+] Emails found:" and parse emails from next strings
@@ -112,63 +112,52 @@ class Domain_dict:
                 flag = 1
         self.__domain_dict.update({"mail_addresses": mail_addresses})
 
-    def find_open_web_servers(self):
+    async def find_open_web_servers(self):
         domain_name = self.__domain_dict["domain_name"]
-        open_ports = Bash_features.check_open_ports_nmap(domain_name, [80,443])
-        web_server_url = ""
-        if 443 in open_ports:
-            web_server_url = f"https://{domain_name}"
-        elif 80 in open_ports:
-            web_server_url = f"http://{domain_name}"
-        else:
-            return
-        web_server_title = Bash_features.parse_title_by_url(web_server_url)
+        web_server_title = await Bash_features.parse_title_by_url(f"https://{domain_name}")
+        if web_server_title == "":
+            web_server_title = await Bash_features.parse_title_by_url(f"http://{domain_name}")
         if len(web_server_title) > 70:
-            web_server_title = web_server_title[0:47] + "..." 
+            web_server_title = web_server_title[0:67] + "..." 
         if web_server_title:
-            self.__domain_dict.update({"web_server_url": web_server_url, "web_server_title": web_server_title})
+            self.__domain_dict.update({"web_server_title": web_server_title})
                 
     def __str__(self):
         domain_name = self.__domain_dict["domain_name"]
         mail_subdomains = self.__domain_dict["mail_subdomains"]
         mail_subdomains_str = "".join([mail_subdomain + " " for mail_subdomain in mail_subdomains])[0:-1]
-        web_server_url = ""
         web_server_title = ""
-        if "web_server_url" in self.__domain_dict:
-            web_server_url = self.__domain_dict["web_server_url"]
+        if "web_server_title" in self.__domain_dict:
             web_server_title = self.__domain_dict["web_server_title"].replace(",", ".")
-        return f"{domain_name},{mail_subdomains_str},{web_server_url},{web_server_title}"
-            
-
-#class Domain_dict_array:
-#    __domain_dict_array = []
-#
-#    def __init__(self, file_name):
-#        with open(file_name) as f:
-#            for line in f:
-#                domain_name = Bash_features.parse_domain_name(line)
-#                if domain_name:
-#                    x = Domain_dict(domain_name)
-#                    self.__domain_dict_array.append(x)
-#    
-#    def __str__(self):
-#        for domain_dict in self.__domain_dict_array:
-#            print(domain_dict)
-#        return "".join(str(domain_name_output) + "\n" for domain_name_output in self.__domain_dict_array)
+        return f"{domain_name},{mail_subdomains_str},{web_server_title}"
         
+async def main():
+    #if len(sys.argv) != 2:
+    #    print("Usage: python script.py file_name")
+    #    exit()
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py file_name")
-        exit()
-    
-    file_name = sys.argv[1]
+    domains = []
+    file_name = "/tmp/1412.txt"
+    #file_name = sys.argv[1]
     try:
         with open(file_name) as f:
             for line in f:
-                domain_name = Bash_features.parse_domain_name(line)
-                if domain_name:
-                    print(Domain_dict(domain_name))
+                domains.append(line)
     except Exception as e:
         print(f"No such file: \"{file_name}\". Try again!")
         exit()
+    app_tasks = [asyncio.create_task(task(line)) for line in domains]
+    await asyncio.gather(*app_tasks)
+
+
+async def task(line):
+    start = time.monotonic()
+    domain_name = Bash_features.parse_domain_name(line)
+    if domain_name:
+        x = Domain_dict(domain_name)
+        await x.find_open_web_servers()
+        print(x)
+    print(time.monotonic() - start)
+
+if __name__ == "__main__":
+    asyncio.run(main())
